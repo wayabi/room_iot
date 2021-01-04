03eaf4491728119682e68ac0404bc4d3021df036
wayabi <wayabi.software@gmail.com>
2021-01-04 12:04:30 +0900

    bluetooth loop

--- script/bluetooth_loop.sh
+++ script/bluetooth_loop.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
-
+#!/bin/bash
+sleep 5
 while [ 1 ]
 do
-	echo -e 'power on\nscan on\n' | bluetoothctl
+	#echo -e 'power on\nscan on\n' | bluetoothctl
+	{
+		printf 'power on\n\n'
+		sleep 2
+		printf 'scan on\n\n'
+		sleep 2
+		printf 'quit\n\n'
+	} | bluetoothctl
 	sleep 3
 done
